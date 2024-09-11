60758656fa61ce25026e559350e408e09627d25e
Chess Seventh <chess7th@pm.me>
2024-09-11 15:00:30 +0200

    feat: improve usage of tailscale by using localapi socket and crate

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,95 @@
 #![allow(clippy::unused_io_amount)]
-use configparser::ini::Ini;
 
 use local_ip_address::list_afinet_netifas;
 
 use once_cell::sync::OnceCell;
+use tailscale_localapi::{LocalApi, PeerStatus, UnixStreamClient};
 
+use std::collections::HashMap;
 use std::env;
-use std::error::Error;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-
 static SERVERS: OnceCell<Vec<Server>> = OnceCell::new();
-
 
 #[derive(Debug, Clone)]
 struct Server {
     ip: String,
     port: u16,
     name: String,
+    online: bool,
 }
 
 impl Server {
-    fn new(ip: &str, port: u16, name: &str) -> Server {
-        Server {
-            ip: ip.to_string(),
-            port,
-            name: name.to_string(),
-        }
-    }
-
     fn connection_string(&self) -> String {
         format!("{}:{}", self.ip, self.port)
     }
 }
 
-
 #[tokio::main]
 async fn main() {
-    let nixos_servers = prep_servers();
-    SERVERS.set(nixos_servers.clone()).unwrap();
+    let local_server = tokio::task::spawn_blocking(get_local_api_servers)
+        .await
+        .expect("Task panicked");
 
-    let _ = whereami().await.unwrap();
+    let clients = retrieve_peers(local_server.await).await;
+    prepare_peers(clients).await;
+}
+
+// async fn get_local_api_servers() -> HashMap<String, PeerStatus> {
+async fn get_local_api_servers() -> LocalApi<UnixStreamClient> {
+    let socket_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "/var/run/tailscale/tailscaled.sock".to_string());
+
+    tailscale_localapi::LocalApi::new_with_socket_path(socket_path)
+}
+
+async fn retrieve_peers(
+    client: tailscale_localapi::LocalApi<tailscale_localapi::UnixStreamClient>,
+) -> HashMap<String, PeerStatus> {
+    let client_status = client.status().await.unwrap();
+    client_status.peer
+}
+
+async fn retrieve_self(
+    client: tailscale_localapi::LocalApi<tailscale_localapi::UnixStreamClient>,
+) -> PeerStatus {
+    let client_status = client.status().await.unwrap();
+    client_status.self_status
+}
+
+async fn prepare_peers(peer: std::collections::HashMap<String, tailscale_localapi::PeerStatus>) {
+    let nixos_servers = get_peers(&peer);
+
+    let _ = whereami(nixos_servers.clone()).await.unwrap();
 
     for server in nixos_servers {
         match_server(server).await;
-    };
+    }
+}
 
+fn get_peers(
+    peer: &std::collections::HashMap<String, tailscale_localapi::PeerStatus>,
+) -> Vec<Server> {
+    peer.iter().map(peer_info).collect::<Vec<Server>>()
+}
+
+fn peer_info(host: (&String, &tailscale_localapi::PeerStatus)) -> Server {
+    Server {
+        ip: host.1.tailscale_ips.first().unwrap().to_string(),
+        port: get_proper_port(&host.1.hostname),
+        name: host.1.hostname.clone(),
+        online: host.1.online,
+    }
+}
+
+fn get_proper_port(hostname: &str) -> u16 {
+    if hostname.contains("bullwackies") {
+        41234
+    } else {
+        22
+    }
 }
 
 fn whoami() -> String {
@@ -58,13 +101,27 @@ fn whoami() -> String {
     }
 }
 
-
 async fn get_pub_ip() -> Result<String, reqwest::Error> {
     reqwest::get("https://api.ipify.org").await?.text().await
-
 }
 
-async fn whereami() -> Result<String, reqwest::Error> {
+async fn whereami(mut nixos_servers: Vec<Server>) -> Result<String, reqwest::Error> {
+    let local_server = tokio::task::spawn_blocking(get_local_api_servers)
+        .await
+        .expect("Task panicked");
+
+    let self_client = retrieve_self(local_server.await).await;
+    let my_client = Server {
+        ip: self_client.tailscale_ips.first().unwrap().to_string(),
+        port: get_proper_port(&self_client.hostname),
+        name: self_client.hostname,
+        online: self_client.online,
+    };
+
+    nixos_servers.push(my_client);
+
+    SERVERS.set(nixos_servers.clone()).unwrap();
+
     let pub_ip = get_pub_ip().await;
     let pub_ip2 = pub_ip.as_ref().unwrap();
 
@@ -72,61 +129,39 @@ async fn whereami() -> Result<String, reqwest::Error> {
     let server = whoami();
     let msg = format!("Server: {server} public IP address is: {pub_ip2:}/32");
 
-    let _ = client.post("https://ntfy.sh/c7-nixos-pings-d34d-b33f")
+    let _ = client
+        .post("https://ntfy.sh/c7-nixos-pings-d34d-b33f")
         .body(msg)
         .header("Title", "NixOS Pinging")
         .header("Priority", "urgent")
         .header("Tags", "rainbow")
         .send()
-    .await?;
+        .await?;
 
     pub_ip
-}
-
-fn prep_servers() -> Vec<Server> {
-    match read_config() {
-        Ok(servers) => servers,
-        Err(e) => {
-            panic!("Error reading config: {e}");
-        },
-    }
-}
-
-fn read_config() -> Result<Vec<Server>, Box<dyn Error>> {
-    let mut config_ini = Ini::new();
-
-    let config_file = format!("{}/.config/rusty-ntfy/rusty-ntfy.ini", env::var("HOME").unwrap());
-    let _config_map = config_ini.load(config_file)?;
-    let servers_names: Vec<String> = config_ini.get("servers", "names").unwrap().split_whitespace().map(std::string::ToString::to_string).collect();
-
-    let vec_servers: Vec<Server> = servers_names.into_iter().map(|server| {
-        let ip = config_ini.get(&server, "ip").expect("Could not read the server IP address from config");
-        let port = config_ini.get(&server, "port").expect("Could not read the server PORT from config").parse::<u16>().expect("Could not parse the port into u16");
-        Server::new(&ip, port, &server)
-    }).collect();
-
-    Ok(vec_servers)
 }
 
 async fn match_server(server: Server) {
     match connect_to_server(&server.clone()) {
         Ok(()) => {
             let _ = send_to_ntfy(server, "has been able to", "rainbow").await;
-        },
+        }
         Err(_e) => {
-            let _ =send_to_ntfy(server, "has NOT been able to", "skull").await;
-        },
+            let _ = send_to_ntfy(server, "has NOT been able to", "skull").await;
+        }
     }
 }
 
 fn connect_to_server(server: &Server) -> std::io::Result<()> {
-    let mut stream = TcpStream::connect(server.connection_string())?;
+    if server.online {
+        let mut stream = TcpStream::connect(server.connection_string())?;
 
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
+        stream.write(&[1])?;
+        stream.read(&mut [0; 128])?;
+    }
+
     Ok(())
 }
-
 
 fn get_ip() -> Option<String> {
     let network_interfaces = list_afinet_netifas();
@@ -143,26 +178,23 @@ fn get_ip() -> Option<String> {
     None
 }
 
-
 async fn send_to_ntfy(dest_server: Server, msg: &str, emoji: &str) -> Result<(), reqwest::Error> {
-
     let my_name = whoami();
 
     if my_name != dest_server.name {
-
         let data_msg = format!("{my_name} {msg} ping {0}", dest_server.name);
         let client = reqwest::Client::new();
 
-        let _ = client.post("https://ntfy.sh/c7-nixos-pings-d34d-b33f")
+        let _ = client
+            .post("https://ntfy.sh/c7-nixos-pings-d34d-b33f")
             .body(data_msg)
             .header("Title", "NIXOS Pinging")
             .header("Priority", "urgent")
             .header("Tags", emoji)
             .send()
-        .await?;
-        return Ok(())
+            .await?;
+        return Ok(());
     }
 
     Ok(())
 }
-
