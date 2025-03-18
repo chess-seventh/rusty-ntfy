5fa6cba99bc7e63b69317b14716e75ca078ff371
Chess Seventh <chess7th@pm.me>
2025-03-18 17:03:46 +0100

    feat: change shell.nix to build project

--- shell.nix
+++ shell.nix
@@ -1,24 +1,47 @@
+# let
+#   moz_overlay = import (builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+#   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+#   ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
+# in
+#   with nixpkgs;
+# stdenv.mkDerivation {
+#   name = "rust";
+#   buildInputs = [ 
+#     openssl 
+#     rustup 
+#     ruststable 
+#     cmake 
+#     zlib 
+#     diesel-cli 
+#     postgresql
+#     cargo
+#   ];
+#
+#   shellHook = ''
+#         export OPENSSL_DIR="${openssl.dev}"
+#         export OPENSSL_LIB_DIR="${openssl.out}/lib"
+#         '';
+# }
+
 let
-  moz_overlay = import (builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
-in
-  with nixpkgs;
-stdenv.mkDerivation {
-  name = "rust";
-  buildInputs = [ 
-    openssl 
-    rustup 
-    ruststable 
-    cmake 
-    zlib 
-    diesel-cli 
-    postgresql
-    cargo
+  # Last updated: 2/26/21. Update as necessary from https://status.nixos.org/...
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/da044451c6a70518db5b730fe277b70f494188f1.tar.gz")) {};
+
+  # Rolling updates, not deterministic.
+  # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.diesel-cli
+    pkgs.postgresql
+
+    # Necessary for the openssl-sys crate:
+    pkgs.openssl
+    pkgs.pkg-config
   ];
 
-  shellHook = ''
-        export OPENSSL_DIR="${openssl.dev}"
-        export OPENSSL_LIB_DIR="${openssl.out}/lib"
-        '';
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
